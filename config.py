f1348a5ffcc28535e087b2afd98a18c39cb5b37f
Shotubhayia <148547126+Shotubhayia@users.noreply.github.com>
2025-02-27 09:00:37 +0530

    Update add your bot details

---
+++ config.py
@@ -0,0 +1,7 @@
+# âœ… Bot Configuration (Yaha Sab Change Kar Sakte Ho)
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+SERVER_NAME = "Fucked by Rostu"
+CHANNEL_NAME = "fucked-by-rostu"
+WEBHOOK_NAME = "Hate op"
+SPAM_MESSAGE = "ðŸ”¥  fucked by Rostu! ðŸ”¥"
+WHITELISTED_USERS = [123456789012345678, 987654321098765432]
