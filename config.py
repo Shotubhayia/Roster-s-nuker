379141735181e4ece2affdb992750ae680a2d755
Roster <148547126+Shotubhayia@users.noreply.github.com>
2025-03-01 22:09:36 +0530

    Update config.py final code

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
-{
-    "TOKEN": "TUMHARA_BOT_TOKEN",
-    "GUILD_ID": 123456789012345678,
-    "CHANNEL_NAME": "nuked-by-bot",
-    "WEBHOOK_MESSAGE": "@everyone Nuked!",
+ {   
+
+    "TOKEN": "Token dal chup chap
+    "GUILD_ID": 1171750625923575819,
+    "CHANNEL_NAME": "fucked by rostu",
+    "WEBHOOK_MESSAGE": "@everyone Fucked by Rostu",
     "SPAM_SPEED": 0.2,
-    "CHANNEL_BATCH": 50
+    "CHANNEL_BATCH": 150,
+    "ROLE_BATCH": 150
 }
