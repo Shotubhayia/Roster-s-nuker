718234a5726e8c60514c2ee38110a3a0e7feadf1
Roster <148547126+Shotubhayia@users.noreply.github.com>
2025-03-01 22:09:52 +0530

    Update config.py final code

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
  {   
 
-    "TOKEN": "Token dal chup chap
+    "TOKEN": "Token dal chup chap"
     "GUILD_ID": 1171750625923575819,
     "CHANNEL_NAME": "fucked by rostu",
     "WEBHOOK_MESSAGE": "@everyone Fucked by Rostu",
